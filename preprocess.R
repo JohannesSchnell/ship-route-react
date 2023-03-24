a68e78f8ea98a745f08334ea265835d0d56abacc
JohannesSchnell <j.schnell@52north.org>
2023-03-24 16:39:25 +0100

    comment preprocess

--- preprocess.R
+++ preprocess.R
@@ -1,10 +1,16 @@
+library("rgdal")
 getwd()
 setwd("")
 #define import file 
 data = read.csv("./import.csv", sep = ";", dec = ",")
+data = read.csv("./Caribic-EGCS data/Ship_4-1-March2020-July2020.csv", sep = ";", dec = ",")
+
+#seperate coordinates in: degree minute hemisphere
 lat = strsplit(data$Latitude, " ")
 lng = strsplit(data$Longitude, " ")
 
+
+#calc and rebuilding to decimal coordinates
 latlng_degree2decimal = function(latlng_list){
   n = as.numeric(sapply(latlng_list, "[[", 1))
   d = as.numeric(sapply(latlng_list, "[[", 2))/60
@@ -16,14 +22,24 @@ latlng_degree2decimal = function(latlng_list){
      as.numeric(-1) -> ne_sw[i] 
     }
   }
- print(n)
- print(d)
- print(ne_sw)
+ #print(n)
+ #print(d)
+ #print(ne_sw)
   return( as.numeric(ne_sw)*(n+d) )
 }
 
-
+#transform lat lng and replace in data
 data$Latitude= latlng_degree2decimal(lat)
 data$Longitude = latlng_degree2decimal(lng)
-#define export name
-write.csv(data, "export.csv")
+
+#save as geojson
+writeOGR(data, dsn = './tmp/out.GeoJSON', layer = "layer_name", driver = "GeoJSON")
+
+#transform into .js file so the content is exported as a variable
+#./data/preprocessed.js is the file to reference in index.js
+# import { jsonData } from "./data/preprocessed.js";
+geojson = readLines("./tmp/out.GeoJSON")
+writeLines(paste("export const jsonData = ", geojson), "./data/preprocessed.js")
+
+#write as csv 
+#write.csv(data, "carl_buettner.csv")
